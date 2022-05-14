2723854a6cdab4d27a98e39ed5b10b557b932527
amendezm <alexis.david.mendez@gmail.com>
2022-05-14 18:02:45 -0400

    Config env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,54 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
-module.exports = {
-  entry: {
-    main: "./src/index.tsx",
-    styles: "./src/styles/index.css",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/i, loader: "babel-loader" },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|icon)$/i,
-        type: "asset/resource",
-      },
+module.exports = () => {
+  // call dotenv and it will return an Object with a parsed key
+  const env = dotenv.config().parsed;
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: {
+      main: "./src/index.tsx",
+      styles: "./src/styles/index.css",
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    output: {
+      filename: "[name].js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/i, loader: "babel-loader" },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|icon)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+      new webpack.DefinePlugin(envKeys),
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
-  devServer: {
-    port: 3000,
-    open: true,
-  },
+    devServer: {
+      port: 3000,
+      open: true,
+    },
+  };
 };
