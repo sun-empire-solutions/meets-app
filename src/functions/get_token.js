d44efb433bbdb87e8a7912f32f644dd29e3ae84b
amendezm <alexis.david.mendez@gmail.com>
2022-05-15 19:16:14 -0400

    Fix cors issue

--- src/functions/get_token.js
+++ src/functions/get_token.js
@@ -1,6 +1,14 @@
 const twilio = require("twilio");
 
 exports.handler = async function (context, event, callback) {
+  // Create a custom Twilio Response
+  const response = new Twilio.Response();
+  // Set the CORS headers to allow Flex to make an error-free HTTP request
+  // to this Function
+  response.appendHeader("Access-Control-Allow-Origin", "*");
+  response.appendHeader("Access-Control-Allow-Methods", "OPTIONS, POST, GET");
+  response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const accessToken = new twilio.jwt.AccessToken(
     context.ACCOUNT_SID,
     context.API_KEY_SID,
@@ -11,7 +19,9 @@ exports.handler = async function (context, event, callback) {
     room: "My Room",
   });
   accessToken.addGrant(videoGrant);
-  return callback(null, {
+  response.appendHeader("Content-Type", "application/json");
+  response.setBody({
     token: accessToken.toJwt(),
   });
+  return callback(null, response);
 };
