b11ad043c7cfa172c1880e0c80ce6ae7f8fe8e42
amendezm <alexis.david.mendez@gmail.com>
2022-05-16 17:47:14 -0400

    Create useRoom hook

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Room, connect as twilioConnect } from "twilio-video";
+import { getAccessToken } from "../api";
+
+const useRoom = () => {
+  const [room, setRoom] = useState<Room | null>(null);
+
+  const connect = async (username: string) => {
+    const accessToken = await getAccessToken(username);
+    const room = await twilioConnect(accessToken);
+    setRoom(room);
+  };
+
+  return { room, connect };
+};
+
+export { useRoom };
