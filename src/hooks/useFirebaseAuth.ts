bae8183e8bba357e8fbbd3ae9cc3bd06607cb2e8
amendezm <alexis.david.mendez@gmail.com>
2022-05-18 11:21:22 -0400

    Add env variables for firebase

---
+++ src/hooks/useFirebaseAuth.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+const useFirebaseAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuthReady, setIsAuthReady] = useState(false);
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+  }, []);
+};
+
+export { useFirebaseAuth };
