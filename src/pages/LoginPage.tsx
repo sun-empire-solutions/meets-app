2791277d0295a76dadb38b1ffa66d57e41752bfb
cteyr123 <cteyr123@gmail.com>
2022-05-19 00:28:04 -0400

    register add

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-
+import { useState, useCallback } from "react";
 import { useFirebaseAuth } from "../hooks/useFirebaseAuth";
 
 const LoginPage = () => {
   const { login, signup } = useFirebaseAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isregister, setRegister] = useState(true);
+
+  const handelClick = useCallback(
+    () => (isregister ? handleLogin : handleSignUp),
+    [isregister]
+  );
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -17,15 +22,24 @@ const LoginPage = () => {
 
   const handleLogin = () => {
     login(email, password);
+    console.log("loggIn");
   };
 
   const handleSignUp = () => {
     signup(email, password);
+    console.log("signUp");
+  };
+
+  const handelClickLink = () => {
+    if (isregister) {
+      return setRegister(false);
+    }
+    return setRegister(true);
   };
 
   return (
     <div className="form-login">
-      <h1>Login</h1>
+      <h1>{isregister ? "Login" : "Register"}</h1>
       <form action="">
         <div className="InputEmail">
           <label htmlFor="InputEmail">Email</label>
@@ -49,13 +63,33 @@ const LoginPage = () => {
             placeholder="Type your password"
           />
         </div>
+
+        {!isregister && (
+          <div className="InputPassword">
+            <label htmlFor="InputPassword">Repeat Password</label>
+            <input
+              type="password"
+              name="repeat-password"
+              className="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Type your password"
+            />
+          </div>
+        )}
+
         <div className="buttons">
-          <button type="submit" onClick={handleLogin}>
-            Sign in
+          <button type="submit" onClick={handelClick}>
+            {isregister ? "Sign in" : "Sign up"}
           </button>
-          <button onClick={handleSignUp}>Sign up</button>
         </div>
       </form>
+      <div className="link-signup">
+        <h3>Or {isregister ? "Sign Up" : "Sign In"} Using</h3>
+        <a href="#" onClick={handelClickLink}>
+          {isregister ? "SIGN UP" : "SIGN IN"}
+        </a>
+      </div>
     </div>
   );
 };
