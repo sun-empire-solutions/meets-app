d432f494a20acd84c2b9a90d43315bd925143005
amendezm <alexis.david.mendez@gmail.com>
2022-05-21 17:57:41 -0400

    Show user photo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,18 @@ import { useFirebaseAuth } from "./../hooks/useFirebaseAuth";
 const Menu = () => {
   const [visibleMenu, setVisibleMenu] = useState(false);
   const { user, logout } = useFirebaseAuth();
-  const userInitials = useMemo(() => user?.email.substring(0, 2), [user]);
+  const userInitials = useMemo(() => {
+    const displayName = user?.providerData?.[0]?.displayName;
+    if (displayName) {
+      const names = displayName.split(" ");
+      return names
+        .map((name) => name.charAt(0))
+        .join("")
+        .substring(0, names.length == 1 ? 1 : 2);
+    }
+    return user?.email.substring(0, 2);
+  }, [user]);
+  const userPhotoUrl = useMemo(() => user?.providerData?.[0]?.photoURL, [user]);
 
   const handleClick = () => {
     logout();
@@ -15,11 +26,16 @@ const Menu = () => {
     <div className="menu">
       <div
         className="menu-trigger"
+        style={
+          userPhotoUrl
+            ? { backgroundImage: `url(${userPhotoUrl})` }
+            : { backgroundColor: "rgb(12, 148, 238)" }
+        }
         onClick={() => {
           setVisibleMenu((visibleMenu) => !visibleMenu);
         }}
       >
-        {userInitials?.toUpperCase()}
+        {userPhotoUrl ? "" : userInitials?.toUpperCase()}
       </div>
       {visibleMenu && (
         <div className="menu-body">
