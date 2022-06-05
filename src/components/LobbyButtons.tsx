76f9740895586a127b283814694e752071d3af20
amendezm <alexis.david.mendez@gmail.com>
2022-06-05 14:31:43 -0400

    Clear tracks when navigate to meeting page

--- src/components/LobbyButtons.tsx
+++ src/components/LobbyButtons.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
 import { TrackButtons } from "./TrackButtons";
 import { Button } from "./Button";
+import { TwilioContext } from "../context/TwilioContext";
 
 const LobbyButtons = () => {
   const navigate = useNavigate();
+  const { clearTracks } = useContext(TwilioContext);
 
   const handleJoinClick = () => {
     navigate("/meeting");
+    clearTracks();
   };
 
   return (
