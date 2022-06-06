9a816e4a86da609b1f074a25426c13190b0e2e87
amendezm <alexis.david.mendez@gmail.com>
2022-06-06 16:59:07 -0400

    Modified classNames hook to accept optional arguments

--- src/hooks/useClassNames.ts
+++ src/hooks/useClassNames.ts
@@ -1,8 +1,19 @@
 const useClassNames = () => {
-  return (classNames: string, classObject: { [key in string]: boolean }) =>
-    `${classNames} ${Object.keys(classObject)
-      .filter((key) => classObject[key])
+  return (...args: Arguments) => {
+    const [firstArg, secondArg] = args;
+
+    const haveMultipleArgs = args.length > 1;
+
+    const first = haveMultipleArgs ? firstArg : "";
+    const second = haveMultipleArgs ? secondArg : firstArg;
+    return `${first} ${Object.keys(second)
+      .filter((key) => second[key])
       .join(" ")}`;
+  };
 };
+
+type Arguments =
+  | [string, { [key in string]: boolean }]
+  | [{ [key in string]: boolean }];
 
 export { useClassNames };
