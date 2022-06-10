8087cc04f9cc3b20d8040be7e0f9131652cb4175
amendezm <alexis.david.mendez@gmail.com>
2022-06-10 09:39:38 -0400

    Use video and audio settings before connecting participants to the room

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,14 +1,28 @@
-import { useState } from "react";
-import { Room, connect as twilioConnect } from "twilio-video";
+import { useContext, useState } from "react";
+import {
+  Room,
+  connect as twilioConnect,
+  createLocalTracks,
+} from "twilio-video";
 
 import { getAccessToken } from "../api";
+import { TwilioContext } from "../context/TwilioContext";
 
 const useRoom = () => {
   const [room, setRoom] = useState<Room | null>(null);
+  const { isAudioEnabled, isVideoEnabled } = useContext(TwilioContext);
 
   const connect = async (username: string) => {
     const accessToken = await getAccessToken(username);
-    const room = await twilioConnect(accessToken);
+
+    const tracks = await createLocalTracks({
+      audio: isAudioEnabled,
+      video: isVideoEnabled ? { facingMode: "user" } : false,
+    });
+
+    const room = await twilioConnect(accessToken, {
+      tracks,
+    });
 
     setRoom(room);
   };
