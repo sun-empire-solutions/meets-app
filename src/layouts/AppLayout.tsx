42108b4f67aaae4aedab3e3a888e5b885c925fd9
cteyr123 <cteyr123@gmail.com>
2022-06-12 12:36:52 +0200

    navBar remove meeting page

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,17 +1,19 @@
 import { Outlet, useLocation } from "react-router-dom";
-
 import { TwilioProvider } from "../containers/TwilioProvider";
 import { NavBar } from "../components/NavBar";
 import { useMemo } from "react";
 
 const AppLayout = () => {
   const location = useLocation();
-  const isSignInView = useMemo(() => location.pathname === "/", [location]);
+  const NavBarView = useMemo(
+    () => location.pathname === "/" || location.pathname === "/meeting",
+    [location]
+  );
 
   return (
     <TwilioProvider>
       <div className="app-layout">
-        {!isSignInView && <NavBar />}
+        {!NavBarView && <NavBar />}
         <Outlet />
       </div>
     </TwilioProvider>
