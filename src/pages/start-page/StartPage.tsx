474f98010654db38899c6edc72e22194ecf41c0f
amendezm <alexis.david.mendez@gmail.com>
2022-06-17 11:32:31 -0400

    Create new start page desing

--- src/pages/start-page/StartPage.tsx
+++ src/pages/start-page/StartPage.tsx
@@ -1,13 +1,34 @@
+import { useMemo, useState } from "react";
+
 import { StartButtons } from "./components/StartButtons";
 
+//@ts-ignore
+import linkImageSrc from "./../../assets/images/meeting-link.png";
+
 const StartPage = () => {
+  const [meetings, setMeetings] = useState([]);
+  const haveMeetings = useMemo(() => meetings.length > 0, [meetings]);
+
   return (
     <div className="start-page">
       <StartButtons />
-      <div className="meetings">
-        <h5 className="title">Meetings</h5>
-        <div className="meeting-list"></div>
-      </div>
+      {haveMeetings ? (
+        <div className="meetings">
+          <h5 className="title">Meetings</h5>
+          <div className="meeting-list"></div>
+        </div>
+      ) : (
+        <div className="info-message">
+          <div className="info-message_image">
+            <img src={linkImageSrc} alt="Link" />
+          </div>
+          <div className="info-message_title">Get a lik you can share</div>
+          <div className="info-message_subtitle">
+            Tap <strong>New meeting</strong> to get a link you can send to
+            people yo want to meet with
+          </div>
+        </div>
+      )}
     </div>
   );
 };
