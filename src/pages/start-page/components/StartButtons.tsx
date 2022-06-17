67895d788711f73c3a44aa2466ef8d87b8ac32b1
amendezm <alexis.david.mendez@gmail.com>
2022-06-17 10:43:49 -0400

    Code review

---
+++ src/pages/start-page/components/StartButtons.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../../../components/Button";
+
+const StartButtons = () => (
+  <div className="start-page_buttons">
+    <Button text="New meeting" classNames="meeting-button" onClick={() => {}} />
+    <Button
+      text="Join with a code"
+      classNames="join-button"
+      onClick={() => {}}
+    />
+  </div>
+);
+
+export { StartButtons };
