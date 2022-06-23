3604e0e63fccf2c27985e317825cfd43dd3a4716
amendezm <alexis.david.mendez@gmail.com>
2022-06-23 17:47:22 -0400

    Set window height to app layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,15 @@
+import { useScreenDimensions } from "use-screen-dimensions";
 import { Outlet } from "react-router-dom";
 
 import { TwilioProvider } from "../containers/TwilioProvider";
 import { NavBar } from "../components/NavBar";
 
 const AppLayout = () => {
+  const { height } = useScreenDimensions();
+
   return (
     <TwilioProvider>
-      <div className="app-layout">
+      <div className="app-layout" style={{ height }}>
         <NavBar />
         <Outlet />
       </div>
