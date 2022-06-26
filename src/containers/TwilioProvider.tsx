f61da8ede739f27b46128673380f461aa030512e
amendezm <alexis.david.mendez@gmail.com>
2022-06-26 15:48:04 -0400

    Show twilio logs in console

--- src/containers/TwilioProvider.tsx
+++ src/containers/TwilioProvider.tsx
@@ -1,7 +1,21 @@
+import { Logger } from "twilio-video";
+
 import { TwilioContext } from "../context/TwilioContext";
 import { useLocalTracks } from "../hooks/useLocalTracks";
 import { useRoom } from "../hooks/useRoom";
 import { useTracksSettings } from "../hooks/useTracksSettings";
+
+const logger = Logger.getLogger("twilio-video");
+
+const originalFactory = logger.methodFactory;
+logger.methodFactory = (methodName, logLevel, loggerName) => {
+  const method = originalFactory(methodName, logLevel, loggerName);
+  return (datetime, logLevel, component, message, data) => {
+    const prefix = "[Meet app]";
+    method(prefix, datetime, logLevel, component, message, data);
+  };
+};
+logger.setLevel("debug");
 
 const TwilioProvider = ({ children }: IProps) => {
   const { room, connect } = useRoom();
