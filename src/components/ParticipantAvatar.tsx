9e7ead1817b0d2b051e663ab5f892dc6dfdcb37f
amendezm <alexis.david.mendez@gmail.com>
2022-06-27 11:01:01 -0400

    Handle photo url load error

--- src/components/ParticipantAvatar.tsx
+++ src/components/ParticipantAvatar.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 import { useAuthUser } from "../hooks/useAuthUser";
@@ -6,11 +6,21 @@ import { useAuthUser } from "../hooks/useAuthUser";
 const ParticipantAvatar = () => {
   const { user } = useAuthUser();
   const userPhotoUrl = useMemo(() => user?.providerData?.[0]?.photoURL, [user]);
+  const [errorOccured, setErrorOccured] = useState(false);
+
+  const handleError = () => {
+    setErrorOccured(true);
+  };
 
   return (
     <div className="avatar">
-      {userPhotoUrl ? (
-        <img className="avatar-image" src={userPhotoUrl} alt="user photo" />
+      {userPhotoUrl && !errorOccured ? (
+        <img
+          className="avatar-image"
+          src={userPhotoUrl}
+          alt="user photo"
+          onError={handleError}
+        />
       ) : (
         <div className="avatar-icon">
           <FaUserCircle />
