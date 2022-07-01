86810a8f1c62ade9720ac9b7ae5033a890ba7ab2
amendezm <alexis.david.mendez@gmail.com>
2022-07-01 14:56:52 -0400

    Add input styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,11 @@
 const Input = ({ classNames, placeholder }: IProps) => {
-  return <input type="text" className={classNames} placeholder={placeholder} />;
+  return (
+    <input
+      type="text"
+      className={`input ${classNames}`}
+      placeholder={placeholder}
+    />
+  );
 };
 
 type IProps = {
