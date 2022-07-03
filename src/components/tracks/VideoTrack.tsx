83471f97a250139e45880a070ce1f49177715a8f
amendezm <alexis.david.mendez@gmail.com>
2022-07-03 19:57:59 -0400

    Create VideoTrack component

---
+++ src/components/tracks/VideoTrack.tsx
@@ -0,0 +1,50 @@
+import { useRef, useEffect } from "react";
+import { Track } from "twilio-video";
+
+import { useMediaStreamTrack, useVideoTrackDimensions } from "../../hooks";
+import { IVideoTrack } from "../../types";
+
+const VideoTrack = ({ track, isLocal, priority }: IProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null!);
+  const mediaStreamTrack = useMediaStreamTrack(track);
+  const dimensions = useVideoTrackDimensions(track);
+  const isPortrait = (dimensions?.height ?? 0) > (dimensions?.width ?? 0);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    videoElement.muted = true;
+    if (track.setPriority && priority) {
+      track.setPriority(priority);
+    }
+    track.attach(videoElement);
+    return () => {
+      track.detach(videoElement);
+
+      videoElement.srcObject = null;
+
+      if (track.setPriority && priority) {
+        track.setPriority(null);
+      }
+    };
+  }, [track, priority]);
+
+  const isFrontFacing =
+    mediaStreamTrack?.getSettings().facingMode !== "environment";
+  const style = {
+    transform: isLocal && isFrontFacing ? "scaleX(-1)" : "",
+    objectFit:
+      isPortrait || track.name.includes("screen")
+        ? ("contain" as const)
+        : ("cover" as const),
+  };
+
+  return <video ref={videoRef} style={style} />;
+};
+
+type IProps = {
+  track: IVideoTrack;
+  isLocal?: boolean;
+  priority?: Track.Priority | null;
+};
+
+export { VideoTrack };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,16 @@
+import { LocalVideoTrack, RemoteVideoTrack } from "twilio-video";
+
+declare module "twilio-video" {
+  // These help to create union types between Local and Remote VideoTracks
+  interface LocalVideoTrack {
+    isSwitchedOff: undefined;
+    setPriority: undefined;
+  }
+}
+
 export type IMeeting = {
   code: string;
   timestamp: string;
 };
+
+export type IVideoTrack = LocalVideoTrack | RemoteVideoTrack;
