a15e798446143aff0752e01b8afd505f616859d5
amendezm <alexis.david.mendez@gmail.com>
2022-07-05 17:08:02 -0400

    Refact LobbyPage

--- src/pages/lobby-page/LobbyPage.tsx
+++ src/pages/lobby-page/LobbyPage.tsx
@@ -1,74 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  createLocalTracks,
-  createLocalVideoTrack,
-  LocalVideoTrack,
-} from "twilio-video";
+import { useEffect } from "react";
 
-import { LobbyButtons, ParticipantAvatar } from "../../components";
+import { LobbyButtons } from "../../components";
 import { useTwilioContext } from "../../context";
-import { useClassNames } from "../../hooks/useClassNames";
+
 import { LocalVideoPreview } from "./components/LocalVideoPreview";
 
 const LobbyPage = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
   const {
-    isVideoEnabled,
-    saveAudioSettings,
-    saveVideoSettings,
-    getAudioSettings,
     getVideoSettings,
+    getLocalVideoTrack,
+    removeLocalVideoTrack,
+    isAcquiringLocalTracks,
   } = useTwilioContext();
-  const [videoTrack, setVideoTrack] = useState<LocalVideoTrack | null>(null);
-  const classNames = useClassNames();
-
-  const saveVideoTrack = (vTrack) => {
-    setVideoTrack((prev) => {
-      cleanPrevVideoTrack(prev, videoRef);
-      return vTrack;
-    });
-  };
-
-  const clearVideoTrack = () => {
-    videoTrack?.stop();
-    videoTrack?.detach(videoRef.current);
-  };
 
   useEffect(() => {
-    const videoSettings = getVideoSettings();
+    const isVideoEnabled = getVideoSettings();
 
-    createLocalTracks({
-      audio: false,
-      video: videoSettings ? { facingMode: "user" } : false,
-    }).then((tracks) => {
-      const vTrack = tracks.find(findVideoTrack) as LocalVideoTrack;
-      vTrack?.attach(videoRef.current);
-      saveVideoTrack(vTrack);
-      saveVideoSettings(vTrack?.isEnabled ?? false);
-      saveAudioSettings(getAudioSettings());
-    });
-  }, []);
-
-  useEffect(() => {
     if (isVideoEnabled) {
-      createLocalVideoTrack({
-        facingMode: "user",
-      }).then((track) => {
-        track?.attach(videoRef.current);
-        saveVideoTrack(track);
-        saveVideoSettings(true);
-      });
-      return;
+      getLocalVideoTrack();
     }
-    clearVideoTrack();
-    saveVideoTrack(null);
-  }, [isVideoEnabled]);
 
-  useEffect(() => {
     return () => {
-      clearVideoTrack();
+      removeLocalVideoTrack();
     };
-  }, [videoTrack]);
+  }, []);
 
   return (
     <div className="container">
@@ -78,15 +33,4 @@ const LobbyPage = () => {
   );
 };
 
-const findVideoTrack = (track) => track.kind === "video";
-
-const cleanPrevVideoTrack = (
-  prev: LocalVideoTrack | null,
-  videoRef: React.MutableRefObject<HTMLVideoElement>
-) => {
-  if (prev) {
-    prev?.stop();
-    prev?.detach(videoRef.current);
-  }
-};
 export { LobbyPage };
