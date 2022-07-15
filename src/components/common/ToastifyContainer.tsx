e94055223a3def4721924193bbdbfc7bdfd443cd
amendezm <alexis.david.mendez@gmail.com>
2022-07-14 20:45:27 -0400

    Create custom toast container

---
+++ src/components/common/ToastifyContainer.tsx
@@ -0,0 +1,8 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastifyContainer = () => {
+  return <ToastContainer position="top-right" />;
+};
+
+export { ToastifyContainer };
