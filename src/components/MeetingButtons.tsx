8a4cc0777a87257850a94a80b1eccb9b5ef9f24d
nitskp <nitinkpandey4@gmail.com>
2022-07-27 20:32:34 +0530

    Got screen track from local user

--- src/components/MeetingButtons.tsx
+++ src/components/MeetingButtons.tsx
@@ -1,19 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdCallEnd } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 import { useTwilioContext } from "../context";
-import { useMeetingCode } from "../hooks";
+import { useMeetingCode, useParticipants } from "../hooks";
 
 import { Button } from "./Button";
 import { TrackButtons } from "./TrackButtons";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import { Twilio } from "twilio";
-import { createLocalTracks, createLocalVideoTrack, LocalVideoTrack } from "twilio-video";
+import { createLocalTracks, createLocalVideoTrack, LocalVideoTrack, LocalParticipant } from "twilio-video";
 
 const MeetingButtons = () => {
   const { room, removeLocalAudioAndVideoTracks } = useTwilioContext();
+  const participants = useParticipants();
   const navigate = useNavigate();
+  const [shareOwnScreen, setOwnShareScreen] = useState(false)
   const { removeMeetingCode } = useMeetingCode();
 
   const handleLeave = () => {
@@ -24,10 +26,12 @@ const MeetingButtons = () => {
 
   const shareScreen = async () => {
     try {
-      const stream = await navigator.mediaDevices.getDisplayMedia();
-      const screenTrack = new LocalVideoTrack(stream.getTracks()[0]);
-      room.localParticipant.publishTrack(screenTrack);
-     console.log('Stream',stream)
+      const stream = await navigator.mediaDevices.getDisplayMedia({video: {frameRate: 15}});
+      const screenTrack = new LocalVideoTrack(stream.getTracks()[0],{name:'screen-share'});
+      console.log('screenTrack',screenTrack)
+      console.log(`Participants before`, participants);
+      const published = await room.localParticipant.publishTrack(screenTrack);
+      setOwnShareScreen(true)
     } catch (error) {
       console.error('Screen share error',error);
       alert('Couldn\'t share the screen');
