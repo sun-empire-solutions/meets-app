7ea3a5ae50c8bb0c352f99857852986e3a8f4320
cteyr123 <cteyr123@gmail.com>
2022-07-30 13:14:07 +0200

    share design button fixed

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,22 +1,30 @@
 import { Button } from "./Button";
-import { BsUpload,BsCheck2 } from "react-icons/bs";
+import { BsCheck2 } from "react-icons/bs";
+import { MdOutlineIosShare } from "react-icons/md";
 import { useState } from "react";
 
 interface Props {
-    meetCode: string;
+  meetCode: string;
 }
 
 export function ShareButton({ meetCode }: Props) {
-    const [codeCopied, setCodeCopied] = useState(false)
-    return (
-        <Button icon={codeCopied?<BsCheck2 color="#26cc00"/>:<BsUpload />}
-            text=""
-            classNames={"share-button"}
-            onClick={() => {
-                navigator.clipboard.writeText(meetCode)
-                setCodeCopied(true)
-                setTimeout(()=>setCodeCopied(false), 1000)
-            }}
-        />
-    )
+  const [codeCopied, setCodeCopied] = useState(false);
+  return (
+    <Button
+      icon={
+        codeCopied ? (
+          <BsCheck2 color="#fff" size={24} />
+        ) : (
+          <MdOutlineIosShare size={24} />
+        )
+      }
+      text=""
+      classNames={"share-button"}
+      onClick={() => {
+        navigator.clipboard.writeText(meetCode);
+        setCodeCopied(true);
+        setTimeout(() => setCodeCopied(false), 1000);
+      }}
+    />
+  );
 }
