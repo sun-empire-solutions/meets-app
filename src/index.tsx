2b412d6d380048b270ed7b747fe4a5c989d9a286
amendezm <alexis.david.mendez@gmail.com>
2022-09-02 15:29:53 -0400

    Remove SW registration

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,3 @@ import { App } from "./App";
 const root = createRoot(document.getElementById("app"));
 
 root.render(<App />);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
